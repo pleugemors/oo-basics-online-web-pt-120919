c6fd4db8e1aa7cd03e6641928889d3e3cfa89d0a
Paul Leugemors <pleugemors@yahoo.com>
2019-12-10 19:01:50 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,38 @@
-
+class Book 
+  def initialize(name)
+    @title = name
+  end
+  
+  def title
+    @title
+  end
+  
+  def author=(name)
+    @author = name
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(style)
+    @genre = style
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
